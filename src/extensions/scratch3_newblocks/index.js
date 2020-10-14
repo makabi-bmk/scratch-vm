f2185f909961c8e346e06195a2bfc1d653e89fe7
makabi <mi161332@edu.okinawa-ct.ac.jp>
2020-10-14 18:47:43 +0900

    コメントアウトしていた行を消した

--- src/extensions/scratch3_newblocks/index.js
+++ src/extensions/scratch3_newblocks/index.js
@@ -78,26 +78,6 @@ class Scratch3NewBlocks {
         const text = Cast.toString(args.TEXT);
         log.log(text);
         
-
-        //mycode
-        // try {
-        //     const con = new WebSocket('ws://localhost:8081/');
-            
-        //     con.onopen = function() {
-        //         console.log('coを開始しました');
-        //         con.send('Hello WebSocket!');
-        //     };
-
-        //     con.onmessage = function(msg) {
-        //         alert(msg.data);
-        //         console.log(msg.data);
-        //     };
-        //     // con.close();
-
-        // } catch (error) {
-        //     console.log(error);
-        // }
-        
     }
 
     /**
